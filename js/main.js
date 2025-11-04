7d39a358ef4b9e81be45dd5383deb1b70b381eae
Kyron Wilson <inf6mous9@gmail.com>
2025-11-04 09:50:54 -0600

    Animation in java

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 // JS scripts placed here
+const button = document.getElementById("clickBtn");
+const stick = document.getElementById("stick");
+
+button.addEventListener("click", () => {
+  button.style.display = "none"; // Hide button
+  stick.classList.add("fly"); // Animate stick figure
+
+  // Redirect after animation ends
+  setTimeout(() => {
+    window.location.href = "next.html"; // Change this to your destination
+  }, 2000);
+});
